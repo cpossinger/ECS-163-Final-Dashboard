b5d56708128ae386218565a78cbd918a233fef81
cpossinger <cposs2000@gmail.com>
2021-03-07 20:52:03 -0800

    Added Layout and StreamGraph emitted values

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -1,163 +1,163 @@
 <!-- Your HTML goes here -->
 <template>
-    <div>
-      <svg class="bar-container" :width="width + 50" :height="height + 50" id="barchart" :viewBox='viewBox' >
-        <g transform="translate(100, 0)" class='bar-vertical'></g>
-        
-        <g transform="translate(100, 500)" class='bar-horizontal'></g>
-        <g class="bars"></g>
-      </svg>
-    </div>
+  <div>
+    <svg class="bar-container" :width="width + 50" :height="height + 50" id="barchart" :viewBox='viewBox' >
+      <g transform="translate(100, 0)" class='bar-vertical'></g>
+
+      <g transform="translate(100, 500)" class='bar-horizontal'></g>
+      <g class="bars"></g>
+    </svg>
+  </div>
 </template>
 
 <script>
 import * as d3 from 'd3'
 const buffer = 100
 export default {
-    name: 'BarChart', // Feel free to rename this and the file
-    props: {
-        dataset: {
-            required: true
-        },
-        width: {
-            required: true
-        },
-        height: {
-            required: true
-        },
-        attribX: {
-            type: String,
-            required: true
-        },
-        attribY: {
-            type: String,
-            required: true
-        },
-        setFilter: {
-            type: Function,
-            required: true
-        }
-        
+  name: 'BarChart', // Feel free to rename this and the file
+  props: {
+    dataset: {
+      required: true
     },
-    data() {
-      return {
-        vertical: null,
-        horizontal: null,
-        selection: [],
-        fixed_data: []
-      }
+    width: {
+      required: true
     },
-    mounted() {
-        this.init()
+    height: {
+      required: true
     },
-    computed: {
-        viewBox() {
-            return `0 0 ${this.width +1.25* buffer} ${this.height+buffer}`;
-        },
+    attribX: {
+      type: String,
+      required: true
     },
-    methods: {
-        
-        renderData() {
-            console.log("bar data")
-            this.total = 0
-            this.fixed_data = []
-            d3.group(this.dataset, d=> {
-              return d[this.attribX]
-            })
-            
-              .forEach(
-                (value, key) => {
-                  console.log("barchart foreach", key, value)
-                  let sum = 0;
-                  for (let i = 0; i < value.length; i++) {
-                    sum += value[i][this.attribY]
-                  }
-                  this.fixed_data.push({bar: value[0][this.attribX], data:sum})
-                  this.total += sum
-                }
-              )
-            this.horizontal = d3.scaleLinear().domain([d3.min(this.fixed_data, d=>d.data), 1.1* d3.max(this.fixed_data, d=>d.data)]).range([0, this.width])
-            this.vertical = d3.scaleBand(this.fixed_data.map(d => d.bar), [0, this.height])
-            this.renderBars()
-            /*
-            bars.append("text")
-                .attr("class", "label")
-                .attr("y", d => this.vertical(d.bar)+this.vertical.bandwidth()/2+4)
-                .attr("x", d => this.horizontal(d.data) + 4 + buffer)
-                .text(d => d.data.toFixed(0))
-        */
-        },
-        
-        renderBars() {
-          d3.select(".bars").selectAll("*").remove()
-            let bars = d3.select(".bars")
-            for (let i = 0; i < this.fixed_data.length; i++) {
-              let d = this.fixed_data[i]
-              console.log("selection length", this.selection.length == 0)
-              let sel = this.selection.length == 0 || this.selection.includes(d.bar)
-              bars.append("rect")
-                  .attr("class", "bar")
-                  .attr("y", this.vertical(d.bar) + 0.1 * this.vertical.bandwidth())
-                  .attr("height", 0.8 * this.vertical.bandwidth())
-                  .attr("x", buffer)
-                  .attr("width", this.horizontal(d.data))
-                  .attr("data", d.bar)
-                  .on("click", d => {
-                    //console.log("mouseclick", d,i, d3.select(d.target).attr("data"))
-                    let clicked = d3.select(d.target).attr("data")
-                    let index = this.selection.indexOf(clicked)
-                    if (index != -1) {
-                     this.selection.splice(index, 1) 
-                    } else {
-                      this.selection.push(clicked)
-                    }
-                    this.renderBars()
-                    this.setFilter(this.attribX, this.selection)
-                  })
-                  .attr("fill", "#CCC")
-                  .filter(() => sel)
-                  .attr("fill", "#377db6")
-              bars.append("text")
-                  .attr("class", "label")
-                  .attr("y", this.vertical(d.bar) + this.vertical.bandwidth()/2 + 4)
-                  .attr("x", this.horizontal(d.data) + 4 + buffer)
-                  .text(d.data.toFixed(2))
-            }  
-        },
-
-        renderAxes() {
-          console.log("bar axes")
-          let vert = d3.axisLeft(this.vertical)
-          d3.select(".bar-vertical").call(vert)
-          let hori = d3.axisBottom(this.horizontal)
-          d3.select(".bar-horizontal").call(hori)
-        },
-
-        init() {
-            d3.select(".barchart").attr("width", this.width).attr("height", this.height)
-            console.log("bar init")
-            this.renderData()
-            this.renderAxes()
-        }
+    attribY: {
+      type: String,
+      required: true
     },
-    watch: {
-      data() {
-        // Chart data was updated by main
-        // Must check that every column in selection is still in the dataset, or remove it otherwise.
-        for (let i = 0; i < this.selection.length; i++) {
-          if (this.data.map(d => d[this.attribX]).includes(this.selection[i])){
-            this.selection.splice(i, 1)
-            i--
-          }
-        }
-          
-        this.init()
-      },
-      attribX() {
-        this.selection = []
-        this.init()
-      }
+    setFilter: {
+      type: Function,
+      required: true
     }
+
+  },
+  data() {
+    return {
+      vertical: null,
+      horizontal: null,
+      selection: [],
+      fixed_data: []
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  computed: {
+    viewBox() {
+      return `0 0 ${this.width +1.25* buffer} ${this.height+buffer}`;
+    },
+  },
+  methods: {
+
+    renderData() {
+      console.log("bar data")
+      this.total = 0
+      this.fixed_data = []
+      d3.group(this.dataset, d=> {
+        return d[this.attribX]
+      })
+
+          .forEach(
+              (value, key) => {
+                console.log("barchart foreach", key, value)
+                let sum = 0;
+                for (let i = 0; i < value.length; i++) {
+                  sum += value[i][this.attribY]
+                }
+                this.fixed_data.push({bar: value[0][this.attribX], data:sum})
+                this.total += sum
+              }
+          )
+      this.horizontal = d3.scaleLinear().domain([d3.min(this.fixed_data, d=>d.data), 1.1* d3.max(this.fixed_data, d=>d.data)]).range([0, this.width])
+      this.vertical = d3.scaleBand(this.fixed_data.map(d => d.bar), [0, this.height])
+      this.renderBars()
+      /*
+      bars.append("text")
+          .attr("class", "label")
+          .attr("y", d => this.vertical(d.bar)+this.vertical.bandwidth()/2+4)
+          .attr("x", d => this.horizontal(d.data) + 4 + buffer)
+          .text(d => d.data.toFixed(0))
+  */
+    },
+
+    renderBars() {
+      d3.select(".bars").selectAll("*").remove()
+      let bars = d3.select(".bars")
+      for (let i = 0; i < this.fixed_data.length; i++) {
+        let d = this.fixed_data[i]
+        console.log("selection length", this.selection.length == 0)
+        let sel = this.selection.length == 0 || this.selection.includes(d.bar)
+        bars.append("rect")
+            .attr("class", "bar")
+            .attr("y", this.vertical(d.bar) + 0.1 * this.vertical.bandwidth())
+            .attr("height", 0.8 * this.vertical.bandwidth())
+            .attr("x", buffer)
+            .attr("width", this.horizontal(d.data))
+            .attr("data", d.bar)
+            .on("click", d => {
+              //console.log("mouseclick", d,i, d3.select(d.target).attr("data"))
+              let clicked = d3.select(d.target).attr("data")
+              let index = this.selection.indexOf(clicked)
+              if (index != -1) {
+                this.selection.splice(index, 1)
+              } else {
+                this.selection.push(clicked)
+              }
+              this.renderBars()
+              this.setFilter(this.attribX, this.selection)
+            })
+            .attr("fill", "#CCC")
+            .filter(() => sel)
+            .attr("fill", "#377db6")
+        bars.append("text")
+            .attr("class", "label")
+            .attr("y", this.vertical(d.bar) + this.vertical.bandwidth()/2 + 4)
+            .attr("x", this.horizontal(d.data) + 4 + buffer)
+            .text(d.data.toFixed(2))
+      }
+    },
+
+    renderAxes() {
+      console.log("bar axes")
+      let vert = d3.axisLeft(this.vertical)
+      d3.select(".bar-vertical").call(vert)
+      let hori = d3.axisBottom(this.horizontal)
+      d3.select(".bar-horizontal").call(hori)
+    },
+
+    init() {
+      d3.select(".barchart").attr("width", this.width).attr("height", this.height)
+      console.log("bar init")
+      this.renderData()
+      this.renderAxes()
+    }
+  },
+  watch: {
+    data() {
+      // Chart data was updated by main
+      // Must check that every column in selection is still in the dataset, or remove it otherwise.
+      for (let i = 0; i < this.selection.length; i++) {
+        if (this.data.map(d => d[this.attribX]).includes(this.selection[i])){
+          this.selection.splice(i, 1)
+          i--
+        }
+      }
+
+      this.init()
+    },
+    attribX() {
+      this.selection = []
+      this.init()
+    }
+  }
 }
 </script>
 
--- src/components/ParallelCoordinateChart.vue
+++ src/components/ParallelCoordinateChart.vue
@@ -1,143 +1,155 @@
 <!-- Your HTML goes here -->
 <template>
-     <svg class='container' :viewBox='viewBox'>
-            <g class='pc-lines'></g>
-            <g class='pc-axes'></g>
-        </svg>
+  <svg class='pc-container' :viewBox='viewBox'>
+    <g class='pc-lines'></g>
+    <g class='pc-axes'></g>
+  </svg>
 </template>
 
 <script>
 import * as d3 from 'd3'
 
 export default {
-    name: 'ParallelCoordinateChart', // Feel free to rename this and the file
-    props: {
-        dataset: {
-            required: true
-        },
-        publisher: {
-            required: true
-        },
-        platform: {
-            required: true
-        },
-        width: {
-            required: true
-        },
-        height: {
-            required: true
-        }
+  name: 'ParallelCoordinateChart', // Feel free to rename this and the file
+  props: {
+    dataset: {
+      required: true
     },
-    data() {
-        return {
-            keys: null,
-            margin: {
-               top: 20,
-               bottom: 20,
-               left: 20,
-               right: 20,
-            }
-        }
+    width: {
+      required: true
     },
-    mounted() {
-        this.init()
-    },
-    computed: {
-        viewBox() {
-            return `0 0 ${this.width} ${this.height}`;
-        },
-    },
-    methods: {
-        init() {
-
-            this.x = null
-            this.y = d3.scalePoint()
-    
-            // Selector. To be implemented at later point.
-            // this.z = d3.scaleSequential()
-
-            this.line = d3.line()
-
-            this.update()
-        },
-        update() {
-            
-            // add columns manually
-            let columns = ['Critic_Score', 'User_Score', 'Total_Shipped', 'Global_Sales']
-            this.dataset = Object.assign(this.dataset, {columns})
-            this.keys = this.dataset.columns
-
-            this.x = new Map(Array.from(this.keys, key => [key, d3.scaleLinear(d3.extent(this.dataset, d => d[key]), 
-                [this.margin.left, this.width - this.margin.right])]))
-
-            this.y = d3.scalePoint(this.keys, [this.margin.top, this.height - this.margin.bottom])
-
-            // this.z = 
-
-            this.line = d3.line()
-                .defined(([, value]) => value != null)
-                .x(([key, value]) => this.x.get(key)(value))
-                .y(([key]) => this.y(key))
-
-            this.render_lines()
-            this.render_axes()
-        },
-        render_axes() {
-            d3.select('.pc-axes')
-                .selectAll('g')
-                .data(this.keys)
-                .join('g')
-                    .attr("transform", d => `translate(0,${this.y(d)})`)
-                    .each((d, i, nodes) => {
-                        d3.select(nodes[i]).call(d3.axisBottom(this.x.get(d)));
-                    })
-                    .call(g => g.append("text")
-                        .attr("x", this.margin.left)
-                        .attr("y", -6)
-                        .attr("text-anchor", "start")
-                        .attr("fill", "black")
-                        .text(d => d))
-                    .call(g => g.selectAll("text")
-                        .clone(true).lower()
-                        .attr("fill", "none")
-                        .attr("stroke-width", 5)
-                        .attr("stroke-linejoin", "round")
-                        .attr("stroke", "white"));
-        },
-        render_lines() {
-            d3.select('.pc-lines')
-                    .attr('fill', 'none')
-                    .attr('stroke-width', 1.5)
-                    .attr('stroke-opacity', 0.4)
-                .selectAll('path')
-                .data(this.dataset)
-                .join("path")
-                    .attr("stroke", 'steelblue')
-                    .attr("d", d => this.line(d3.cross(this.keys, [d], (key, d) => [key, d[key]])))
-                .append("title")
-                    .text(d => d.name);
-        },
-        // brushed function taken from brushable scatterplot located at https://observablehq.com/@d3/brushable-scatterplot
-        // copied from project 4, to be repurposed
-        brushed({selection}) {
-            let value = [];
-            if (selection) {
-                const [[x0, y0], [x1, y1]] = selection;
-                value = d3.select('.points').selectAll('circle')
-                    .style('stroke-width', 1.3)
-                    .filter(d => x0 <= this.x(d.stats.attack) && this.x(d.stats.attack) < x1 && y0 <= this.y(d.stats.defense) && this.y(d.stats.defense) < y1)
-                    .style('stroke-width', 2.5)
-                    .data();
-              
-            } else {
-                d3.select('.points').selectAll('circle')
-                    .style('stroke-width', 1.3)
-            }
-            d3.select('.sp_container')
-                .property("value", value).dispatch("input");
-            this.$emit('selected_pokemon', value)
-        }
+    height: {
+      required: true
     }
+  },
+  data() {
+    return {
+      keys: null,
+      selections: null,
+      margin: {
+        top: 20,
+        bottom: 20,
+        left: 20,
+        right: 20,
+      }
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  computed: {
+    viewBox() {
+      return `0 0 ${this.width} ${this.height}`;
+    },
+  },
+  methods: {
+    init() {
+      this.dataset = this.dataset.slice(0, 500)
+      console.log(this.dataset)
+
+      this.x = null
+      this.y = d3.scalePoint()
+      this.brush = d3.brushX()
+
+      // Selector. To be implemented at later point.
+      // this.z = d3.scaleSequential()
+
+      this.line = d3.line()
+
+      this.update()
+    },
+    update() {
+
+      // add columns manually
+      let columns = ['Critic_Score', 'User_Score', 'Total_Shipped', 'Global_Sales']
+      this.dataset = Object.assign(this.dataset, {columns})
+      this.keys = this.dataset.columns
+      console.log(this.keys)
+      this.selections = new Map()
+      console.log(this.selections)
+
+      this.x = new Map(Array.from(this.keys, key => [key, d3.scaleLinear(d3.extent(this.dataset, d => d[key]),
+          [this.margin.left, this.width - this.margin.right])]))
+      console.log(this.x)
+
+      this.y = d3.scalePoint(this.keys, [this.margin.top, this.height - this.margin.bottom])
+      console.log(this.y)
+
+      this.brush = d3.brushX()
+          .extent([
+            [this.margin.left, -(50 / 2)],
+            [this.width - this.margin.right, 50 / 2]
+          ])
+          .on("start brush end", this.brushed);
+
+      d3.select('.pc_container')
+          .call(this.brush)
+
+      // this.z =
+
+      this.line = d3.line()
+          .defined(([, value]) => value != null)
+          .x(([key, value]) => this.x.get(key)(value))
+          .y(([key]) => this.y(key))
+      console.log(this.line)
+
+      this.render_lines()
+      this.render_axes()
+    },
+    render_axes() {
+      d3.select('.pc-axes')
+          .selectAll('g')
+          .data(this.keys)
+          .join('g')
+          .attr("transform", d => `translate(0,${this.y(d)})`)
+          .each((d, i, nodes) => {
+            d3.select(nodes[i]).call(d3.axisBottom(this.x.get(d)));
+          })
+          .call(g => g.append("text")
+              .attr("x", this.margin.left)
+              .attr("y", -6)
+              .attr("text-anchor", "start")
+              .attr("fill", "black")
+              .text(d => d))
+          .call(g => g.selectAll("text")
+              .clone(true).lower()
+              .attr("fill", "none")
+              .attr("stroke-width", 5)
+              .attr("stroke-linejoin", "round")
+              .attr("stroke", "white"))
+          .call(this.brush)
+    },
+    render_lines() {
+      d3.select('.pc-lines')
+          .attr('fill', 'none')
+          .attr('stroke-width', 1.5)
+          .attr('stroke-opacity', 0.4)
+          .selectAll('path')
+          .data(this.dataset)
+          .join("path")
+          .attr("stroke", 'steelblue')
+          .attr("d", d => this.line(d3.cross(this.keys, [d], (key, d) => [key, d[key]])))
+          .append("title")
+          .text(d => d.name);
+    },
+    // brushed function taken from brushable scatterplot located at https://observablehq.com/@d3/brushable-parallel-coordinates
+    brushed({selection}, key) {
+      if (selection === null) this.selections.delete(key);
+      else this.selections.set(key, selection.map(this.x.get(key).invert));
+      const selected = [];
+      d3.select('.pc-lines')
+          .selectAll('path')
+          .each((d, i, nodes) => {
+            const active = Array.from(this.selections).every(([key, [min, max]]) => d[key] >= min && d[key] <= max);
+            d3.select(nodes[i]).style("stroke", active ? 'steelblue' : '#ddd');
+            if (active) {
+              d3.select(nodes[i]).raise();
+              selected.push(d);
+            }
+          })
+          .property("value", selected).dispatch("input");
+    }
+  }
 }
 </script>
 
